656b3429f3f13dbc168fb5be515a7b9e8eb85d71
F.Javier <javijimenez1108@gmail.com>
2018-05-10 11:40:27 +0200

    Empezando elbot

--- bot/prueba.py
+++ bot/prueba.py
@@ -3,6 +3,8 @@
 import telebot # Librería de la API del bot.
 from telebot import types # Tipos para la API del bot.
 import time # Librería para hacer que el programa que controla el bot no se acabe.
+from pysnmp.hlapi import *
+
  
  
 # Aqui definiremos aparte del Token, por ejemplo los ids de los grupos y pondríamos grupo= -XXXXX 
@@ -11,7 +13,7 @@ TOKEN = '563209742:AAHsXl1ZoUIZDYCBTR52oxezfxHKdCoDb8E' # Nuestro token del bot.
  
 AYUDA = 'Puedes utilizar los siguientes comandos : \n\n/ayuda - Guia para utilizar el bot. \n/info - Informacion De interes \n/hola - Saludo del Bot \n/piensa3D - Informacion sobre Piensa3D \n\n'
  
-GRUPO = -XXXXXX #Definimos que cuando pongamos la palabra grupo lo vincule con el Id del grupo donde nos encontremos.  Al meter el bot en un grupo, en la propia consola nos saldrá
+GRUPO = -1001201542913 #Definimos que cuando pongamos la palabra grupo lo vincule con el Id del grupo donde nos encontremos.  Al meter el bot en un grupo, en la propia consola nos saldrá
  
  
  
@@ -41,3 +43,56 @@ def command_ayuda(m): # Definimos una función que resuleva lo que necesitemos.
     bot.send_chat_action(cid, 'typing') # Enviando ...
     time.sleep(1) #La respuesta del bot tarda 1 segundo en ejecutarse
     bot.send_message( cid, AYUDA) # Con la función 'send_message()' del bot, enviamos al ID almacenado el texto que queremos.
+
+@bot.message_handler(commands=['info']) # Indicamos que lo siguiente va a controlar el comando '/info'
+def command_info(m): # Definimos una función que resuleva lo que necesitemos.
+    cid = m.chat.id # Guardamos el ID de la conversación para poder responder.
+    if cid == GRUPO:
+        
+            bot.send_message( GRUPO, 'mensaje A') # Con la función 'send_message()' del bot, enviamos al ID almacenado el texto que queremos.
+    else :
+            bot.send_message( cid, 'mensaje B')
+
+@bot.message_handler(commands=['get'])
+def command_get(m):
+    
+    cid = m.chat.id
+    puerto=161
+    ip = '10.10.10.1'
+    community = "public"
+    mib = 'SNMPv2-MIB'
+    oidObjeto = 'snmpInPkts'
+    oidInstancia = 0
+
+
+
+
+    errorIndication, errorStatus, errorIndex, varBinds = next(
+        getCmd(SnmpEngine(),
+            CommunityData(community, mpModel=0),
+            UdpTransportTarget((ip, puerto)),
+            ContextData(),
+            ObjectType(ObjectIdentity(mib, oidObjeto, oidInstancia).addAsn1MibSource('file:///usr/share/snmp',
+                                                                                 'http://mibs.snmplabs.com/asn1/@mib@')))) #el 0 es porque es la instancia, 
+                                                        #para tablas poner donde esta el 0 el oid del objeto columna
+    
+    if cid == GRUPO:
+        
+            bot.send_message( GRUPO, 'mensaje A') # Con la función 'send_message()' del bot, enviamos al ID almacenado el texto que queremos.
+    else :
+            bot.send_message( cid, 'tusmuertos')
+                                                                      
+
+    if errorIndication:
+        print(errorIndication)
+    elif errorStatus:
+        print('%s at %s' % (errorStatus.prettyPrint(),
+                        errorIndex and varBinds[int(errorIndex) - 1][0] or '?'))
+    else:
+        for varBind in varBinds:
+            bot.send_chat_action(cid, 'typing') # Enviando ...
+            time.sleep(1)
+            bot.send_message(cid,' = '.join([x.prettyPrint() for x in varBind]))
+
+
+bot.polling()
