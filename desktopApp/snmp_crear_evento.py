92715f990e5320bf82418be2bc26b0db39100528
Miguel Angel Cabrera Miñagorri <miguelangelcabrera97@gmail.com>
2018-05-12 21:28:42 +0200

    Convertidos ficheros a funciones para llamar desde el bot

--- desktopApp/snmp_crear_evento.py
+++ desktopApp/snmp_crear_evento.py
@@ -1,44 +1,47 @@
 from pysnmp.hlapi import *
 
+##########################################################################3
+def snmp_crear_evento(ip,community,eventDescription,eventType,eventCommunity,eventOwner,eventIndex):
 
-puerto = 161
+	mib = 'RMON-MIB'
+	puerto = 161
+
+	errorIndication, errorStatus, errorIndex, varBinds = next(
+			setCmd(SnmpEngine(),
+	         	CommunityData(community),
+	            UdpTransportTarget((ip, puerto)),
+	            ContextData(),
+	            ObjectType(ObjectIdentity(mib, 'eventStatus', eventIndex), 1).addAsn1MibSource('file:///usr/share/snmp', #lo ponemos en 2 underCreate
+	                                                                                 'http://mibs.snmplabs.com/asn1/@mib@'),
+				   ObjectType(ObjectIdentity(mib, 'eventDescription', eventIndex), eventDescription).addAsn1MibSource('file:///usr/share/snmp',
+	                                                                                 'http://mibs.snmplabs.com/asn1/@mib@'),
+				   ObjectType(ObjectIdentity(mib, 'eventType', eventIndex), eventType).addAsn1MibSource('file:///usr/share/snmp',
+	                                                                                 'http://mibs.snmplabs.com/asn1/@mib@'),
+				   ObjectType(ObjectIdentity(mib, 'eventOwner', eventIndex), eventOwner).addAsn1MibSource('file:///usr/share/snmp',
+	                                                                                 'http://mibs.snmplabs.com/asn1/@mib@')))
+
+
+	if errorIndication:
+	    print(errorIndication)
+	elif errorStatus:
+	    print('%s at %s' % (errorStatus.prettyPrint(),
+	                        errorIndex and varBinds[int(errorIndex) - 1][0] or '?'))
+
+	return varBinds
+##########################################################################3
+
+
 
 ip = raw_input("Introduzca IP: ")
 community = raw_input("Introduzca community: ") 
-mib = 'RMON-MIB'
-#idObjeto = raw_input("Introduzca Id del objeto: ")
-#oidInstancia = raw_input("Introduzca oid de la instancia: ")
-#value = raw_input("Introduzca el nuevo valor: ")
-
-#eventIndex 	=raw_input("Introduzca eventIndex")
 eventDescription =raw_input("Introduzca eventDescription:")
 eventType =raw_input("Introduzca eventType:")
 eventCommunity =raw_input("Introduzca eventCommunity:")
-#eventLastTimeSent =raw_input("Introduzca eventLastTimeSent:")
 eventOwner =raw_input("Introduzca eventOwner:")
 eventIndex = raw_input("INtroduzca indice evento: ")
-#eventStatus =raw_input("Introduzca eventStatus:")
 
-errorIndication5, errorStatus5, errorIndex5, varBinds5 = next(
-		setCmd(SnmpEngine(),
-         	CommunityData(community),
-            UdpTransportTarget((ip, puerto)),
-            ContextData(),
-            ObjectType(ObjectIdentity(mib, 'eventStatus', eventIndex), 1).addAsn1MibSource('file:///usr/share/snmp', #lo ponemos en 2 underCreate
-                                                                                 'http://mibs.snmplabs.com/asn1/@mib@'),
-			   ObjectType(ObjectIdentity(mib, 'eventDescription', eventIndex), eventDescription).addAsn1MibSource('file:///usr/share/snmp',
-                                                                                 'http://mibs.snmplabs.com/asn1/@mib@'),
-			   ObjectType(ObjectIdentity(mib, 'eventType', eventIndex), eventType).addAsn1MibSource('file:///usr/share/snmp',
-                                                                                 'http://mibs.snmplabs.com/asn1/@mib@'),
-			   ObjectType(ObjectIdentity(mib, 'eventOwner', eventIndex), eventOwner).addAsn1MibSource('file:///usr/share/snmp',
-                                                                                 'http://mibs.snmplabs.com/asn1/@mib@')))
+##LLAmamoe e imprimimos el resultado
+varBinds = snmp_crear_evento(ip,community,eventDescription,eventType,eventCommunity,eventOwner,eventIndex)
 
-
-if errorIndication5:
-    print(errorIndication5)
-elif errorStatus5:
-    print('%s at %s' % (errorStatus5.prettyPrint(),
-                        errorIndex5 and varBinds5[int(errorIndex5) - 1][0] or '?'))
-else:
-    for varBind in varBinds5:
-        print(' = '.join([x.prettyPrint() for x in varBind]))
+for varBind in varBinds:
+    print(' = '.join([x.prettyPrint() for x in varBind]))
