9c8cf5ca02ad1925a090833f73b1ae2e0fd44497
F.Javier <javijimenez1108@gmail.com>
2018-05-13 22:45:24 +0200

    Hecho receptor de traps

---
+++ bot/recibir_traps.py
@@ -0,0 +1,102 @@
+from pysnmp.carrier.asynsock.dispatch import AsynsockDispatcher
+from pysnmp.carrier.asynsock.dgram import udp, udp6
+from pyasn1.codec.ber import decoder
+from pysnmp.proto import api
+import telebot 
+from telebot import types 
+
+
+
+TOKEN = '563209742:AAHsXl1ZoUIZDYCBTR52oxezfxHKdCoDb8E' # Nuestro token del bot.
+
+bot = telebot.TeleBot(TOKEN) # Creamos el objeto de nuestro bot.
+
+def cbFun(transportDispatcher, transportDomain, transportAddress, wholeMsg):
+    while wholeMsg:
+        msgVer = int(api.decodeMessageVersion(wholeMsg))
+        if msgVer in api.protoModules:
+            pMod = api.protoModules[msgVer]
+        else:
+            print('Unsupported SNMP version %s' % msgVer)
+            return
+        reqMsg, wholeMsg = decoder.decode(
+            wholeMsg, asn1Spec=pMod.Message(),
+            )
+        print('Notification message from %s:%s: ' % (
+            transportDomain, transportAddress
+            )
+        )
+        reqPDU = pMod.apiMessage.getPDU(reqMsg)
+        if reqPDU.isSameTypeWith(pMod.TrapPDU()):
+            if msgVer == api.protoVersion1:
+                print('Enterprise: %s' % (
+                    pMod.apiTrapPDU.getEnterprise(reqPDU).prettyPrint()
+                    )
+                )
+                print('Agent Address: %s' % (
+                    pMod.apiTrapPDU.getAgentAddr(reqPDU).prettyPrint()
+                    )
+                )
+                print('Generic Trap: %s' % (
+                    pMod.apiTrapPDU.getGenericTrap(reqPDU).prettyPrint()
+                    )
+                )
+                print('Specific Trap: %s' % (
+                    pMod.apiTrapPDU.getSpecificTrap(reqPDU).prettyPrint()
+                    )
+                )
+                print('Uptime: %s' % (
+                    pMod.apiTrapPDU.getTimeStamp(reqPDU).prettyPrint()
+                    )
+                )
+                varBinds = pMod.apiTrapPDU.getVarBindList(reqPDU)
+            else:
+                varBinds = pMod.apiPDU.getVarBindList(reqPDU)
+            print('Var-binds:')
+            f = open("trap.txt")
+            cid = f.read()
+            f.close()
+            for oid, val in varBinds:
+                print('%s = %s' % (oid, val))
+                bot.send_message(cid, oid + " = " + val)
+
+    return wholeMsg
+
+
+ip = raw_input("(NO OLVIDE CONFIGURAR LA IP A LA QUE EL CONMUUTADOR ENVIA LOS TRAPS) Introduzca ip del servidor para recivir traps: ")
+
+transportDispatcher = AsynsockDispatcher()
+
+transportDispatcher.registerRecvCbFun(cbFun)
+
+# UDP/IPv4
+transportDispatcher.registerTransport(
+    udp.domainName, udp.UdpSocketTransport().openServerMode((ip, 162))
+)
+
+# UDP/IPv6
+transportDispatcher.registerTransport(
+    udp6.domainName, udp6.Udp6SocketTransport().openServerMode(('::1', 162))
+)
+
+transportDispatcher.jobStarted(1)
+
+try:
+    # Dispatcher will never finish as job#1 never reaches zero
+    transportDispatcher.runDispatcher()
+except:
+    transportDispatcher.closeDispatcher()
+    raise
+
+
+
+
+
+
+
+
+
+
+
+
+
